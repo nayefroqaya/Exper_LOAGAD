9179163395722d1bdf86f831d470345a9c8dbf6f
nayefroqaya <n.roqaya@gmail.com>
2026-01-07 19:47:23 +0100

    check label

--- logadempirical/trainer.py
+++ logadempirical/trainer.py
@@ -57,7 +57,7 @@ class Trainer:
         # return 0
         for idx, batch in enumerate(train_loader):
             # batch = {k: v.to(device) for k, v in batch.items()}
-            print(batch)
+            print(batch_size)
             exit()
             outputs = self.model(batch, device=device)
             loss = outputs.loss
