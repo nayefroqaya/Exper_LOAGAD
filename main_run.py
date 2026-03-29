abac9474380109ff9790a9728f21ddea65b1cf3e
nayefroqaya <n.roqaya@gmail.com>
2026-03-29 12:15:25 +0200

    update info

--- main_run.py
+++ main_run.py
@@ -423,9 +423,9 @@ def run(args):
     #file_path_test = 'dataset/HDFS/1_HDFS_Splitted_Datasets/test_df.pkl'
     #file_path_val = 'dataset/HDFS/1_HDFS_Splitted_Datasets/val_df.pkl'
 
-    file_path_train = '../NovaAD_Plus/datasets/SP_100MB/2_SP_100MB_Splitted_Datasets/train_df.pkl'
-    file_path_test = '../NovaAD_Plus/datasets/SP_100MB/2_SP_100MB_Splitted_Datasets/test_df.pkl'
-    file_path_val = '../NovaAD_Plus/datasets/SP_100MB/2_SP_100MB_Splitted_Datasets/val_df.pkl'
+    file_path_train = '../NovaAD_Plus/datasets/SP_100MB/3_SP_100MB_Splitted_Datasets/train_df.pkl'
+    file_path_test = '../NovaAD_Plus/datasets/SP_100MB/3_SP_100MB_Splitted_Datasets/test_df.pkl'
+    file_path_val = '../NovaAD_Plus/datasets/SP_100MB/3_SP_100MB_Splitted_Datasets/val_df.pkl'
 
     # Read pickle file
     df_train = pd.read_pickle(file_path_train)
