8fb6facf1bf636e5f8d1132494073b6daea4355b
nayefroqaya <n.roqaya@gmail.com>
2026-04-04 00:20:17 +0200

    update info

--- main_run.py
+++ main_run.py
@@ -423,9 +423,9 @@ def run(args):
     #file_path_test = 'dataset/BGL/1_BGL_Splitted_Datasets/test_df.pkl'
     #file_path_val = 'dataset/BGL/1_BGL_Splitted_Datasets/val_df.pkl'
 
-    file_path_train = '../NovaAD_Plus/datasets/TH_1G/1_TH_1G_Splitted_Datasets/train_df.pkl'
-    file_path_test = '../NovaAD_Plus/datasets/TH_1G/1_TH_1G_Splitted_Datasets/test_df.pkl'
-    file_path_val = '../NovaAD_Plus/datasets/TH_1G/1_TH_1G_Splitted_Datasets/val_df.pkl'
+    file_path_train = '../NovaAD_Plus/datasets/SP_100MB/1_SP_100MB_Splitted_Datasets/train_df.pkl'
+    file_path_test = '../NovaAD_Plus/datasets/SP_100MB/1_SP_100MB_Splitted_Datasets/test_df.pkl'
+    file_path_val = '../NovaAD_Plus/datasets/SP_100MB/1_SP_100MB_Splitted_Datasets/val_df.pkl'
 
 
 
