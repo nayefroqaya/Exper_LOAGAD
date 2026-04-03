9b83622fe7e477fa7cbd44acf0df1443df5da1ef
nayefroqaya <n.roqaya@gmail.com>
2026-04-03 23:47:57 +0200

    update info

--- main_run.py
+++ main_run.py
@@ -419,13 +419,13 @@ def run(args):
     args.output_dir = output_subdir
 
     # first paper
-    file_path_train = 'dataset/BGL/1_BGL_Splitted_Datasets/train_df.pkl'
-    file_path_test = 'dataset/BGL/1_BGL_Splitted_Datasets/test_df.pkl'
-    file_path_val = 'dataset/BGL/1_BGL_Splitted_Datasets/val_df.pkl'
+    #file_path_train = 'dataset/BGL/1_BGL_Splitted_Datasets/train_df.pkl'
+    #file_path_test = 'dataset/BGL/1_BGL_Splitted_Datasets/test_df.pkl'
+    #file_path_val = 'dataset/BGL/1_BGL_Splitted_Datasets/val_df.pkl'
 
-    #file_path_train = '../NovaAD_Plus/datasets/HDFS/3_HDFS_Splitted_Datasets/train_df.pkl'
-    #file_path_test = '../NovaAD_Plus/datasets/HDFS/3_HDFS_Splitted_Datasets/test_df.pkl'
-    #file_path_val = '../NovaAD_Plus/datasets/HDFS/3_HDFS_Splitted_Datasets/val_df.pkl'
+    file_path_train = '../NovaAD_Plus/datasets/HDFS/3_HDFS_Splitted_Datasets/train_df.pkl'
+    file_path_test = '../NovaAD_Plus/datasets/HDFS/3_HDFS_Splitted_Datasets/test_df.pkl'
+    file_path_val = '../NovaAD_Plus/datasets/HDFS/3_HDFS_Splitted_Datasets/val_df.pkl'
 
     # Read pickle file
     df_train = pd.read_pickle(file_path_train)
