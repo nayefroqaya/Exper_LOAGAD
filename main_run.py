59425490917dfc1833fc6e1696822842deafcd7f
nayefroqaya <n.roqaya@gmail.com>
2026-04-11 13:10:36 +0200

    reformat the code

--- main_run.py
+++ main_run.py
@@ -423,9 +423,9 @@ def run(args):
     #file_path_test = 'dataset/BGL/1_BGL_Splitted_Datasets/test_df.pkl'
     #file_path_val = 'dataset/BGL/1_BGL_Splitted_Datasets/val_df.pkl'
 
-    file_path_train = '../NovaAD_Plus/datasets/BGL/1_BGL_Splitted_Datasets/train_df.pkl'
-    file_path_test = '../NovaAD_Plus/datasets/BGL/1_BGL_Splitted_Datasets/test_df.pkl'
-    file_path_val = '../NovaAD_Plus/datasets/BGL/1_BGL_Splitted_Datasets/val_df.pkl'
+    file_path_train = '../NovaAD_Plus/datasets/BGL/2_BGL_Splitted_Datasets/train_df.pkl'
+    file_path_test = '../NovaAD_Plus/datasets/BGL/2_BGL_Splitted_Datasets/test_df.pkl'
+    file_path_val = '../NovaAD_Plus/datasets/BGL/2_BGL_Splitted_Datasets/val_df.pkl'
 
 
 
